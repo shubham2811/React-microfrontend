f559737e7a66d86c23ef44a193ae7eb70c7d5d47
gurpreet singh <gurpreetsingh@gurpreets-MacBook-Pro.local>
2020-10-29 15:59:17 +0530

    feat: prettier eslint husky docs: update README

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import loadable from '@loadable/component';
+import PropTypes from 'prop-types';
 
 import Login from './containers/Login/Login';
 import NoMatch from './components/NoMatch';
@@ -13,33 +9,33 @@ import Loader from './components/Loader';
 import ApplicationError from './components/ErrorPages/ApplicationError';
 import { routes } from './config/constants';
 
-const Main = loadable(() => import(/* webpackChunkName: "Main" */
-  './containers/Main'), {
-  fallback: <Loader loading={true} size='lg' />
-})
+const Main = loadable(() => import(/* webpackChunkName: "Main" */ './containers/Main'), {
+  fallback: <Loader loading size="lg" />,
+});
 
 // A wrapper for <Route> that redirects to the login
 // screen if you're not yet authenticated.
 const PrivateRoute = ({ children, ...rest }) => {
-  let isAuthenticated = localStorage.getItem('isAuthenticated');
+  const isAuthenticated = localStorage.getItem('isAuthenticated');
   return (
     <Route
+      // eslint-disable-next-line react/jsx-props-no-spreading
       {...rest}
       render={({ location }) =>
-        isAuthenticated === "true" ? (
+        isAuthenticated === 'true' ? (
           children
         ) : (
-            <Redirect
-              to={{
-                pathname: routes.LOGIN,
-                state: { from: location }
-              }}
-            />
-          )
+          <Redirect
+            to={{
+              pathname: routes.LOGIN,
+              state: { from: location },
+            }}
+          />
+        )
       }
     />
   );
-}
+};
 
 const App = () => {
   return (
@@ -63,6 +59,10 @@ const App = () => {
       </Switch>
     </Router>
   );
-}
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.element,
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
 import 'core-js';
-import "regenerator-runtime/runtime.js";
+import 'regenerator-runtime/runtime';
 
 import store from './middleware/Store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import "./style/styles.css";
-import "./style/all.min.css";
-import "./style/custom.css";
-import "toastr/build/toastr.css";
+import './style/styles.css';
+import './style/all.min.css';
+import './style/custom.css';
+import 'toastr/build/toastr.css';
 
-ReactDOM.render(<Provider store={store}>
-  <App />
-</Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
